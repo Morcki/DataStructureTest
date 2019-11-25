a28f6ef608778bab2f7b742e61fc3648e9de08c8
Mocki <cocolytrees@gmail.com>
2019-11-25 20:53:36 +0800

    main file

---
+++ MySTD/test.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{	
+	Test t;
+	t.hannotaTest(4);
+	t.linkdequeTest();
+	t.linkbintreeTest();
+	t.huffmantreeTest();
+}
